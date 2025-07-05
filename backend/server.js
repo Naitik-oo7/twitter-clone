6aea02e12ee5269ceb339c976396842a471269f6
naitik <naitik.koladiya@gmail.com>
2025-07-05 16:17:23 +0530

    changed

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRouter.js";
 import postRouter from "./routes/postRoutes.js";
 import notificationRouter from "./routes/notificationRoutes.js";
+import listEndpoints from "express-list-endpoints";
 
 dotenv.config();
 import { connectMongoDB } from "./db/connectMongoDB.js";
@@ -22,12 +23,13 @@ const app = express();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" })); //to parse form data url encoded
@@ -38,14 +40,10 @@ app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/notifications", notificationRouter);
 
-console.log("Dumping all registered route paths:");
-app._router.stack.forEach(layer => {
-  if (layer.route) {
-    console.log("Route path:", layer.route.path);
-  } else if (layer.name === "router") {
-    console.log("Router middleware regexp:", layer.regexp);
-  }
+console.log("Registered routes:", listEndpoints(app));
+app.use((err, req, res, next) => {
+  console.error("🔥 Global Error:", err.stack);
+  res.status(500).json({ error: "Internal server error" });
 });
-
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
