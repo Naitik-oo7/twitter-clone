f3e6dc0bb5d8b48af9a417ac2fe7d7aa55c050f8
naitik <naitik.koladiya@gmail.com>
2025-07-05 17:02:33 +0530

    error solved

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -113,7 +113,7 @@ const Post = ({ post }) => {
     <div className="flex gap-2 items-start p-4 border-b border-gray-700">
       <div className="avatar">
         <Link
-          to={`/profile/${postOwner.username}`}
+          to={`/profile/${postOwner?.username}`}
           className="w-8 rounded-full overflow-hidden"
         >
           <img src={postOwner?.profileImg || "/avatar-placeholder.png"} />
@@ -196,13 +196,13 @@ const Post = ({ post }) => {
                         <div className="flex flex-col">
                           <div className="flex items-center gap-1">
                             <span className="font-bold">
-                              {comment.user.fullName}
+                              {comment?.user?.fullName}
                             </span>
                             <span className="text-gray-700 text-sm">
-                              @{comment.user.username}
+                              @{comment?.user?.username}
                             </span>
                           </div>
-                          <div className="text-sm">{comment.text}</div>
+                          <div className="text-sm">{comment?.text}</div>
                         </div>
                       </div>
                     ))
